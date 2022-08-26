9c20cfb2450980fe883e529d18e61bb39174de6f
BenD2525 <bendawson2525@gmail.com>
2022-08-26 10:36:01 +0000

    Add signup functionality and link to success page

--- HealthHub/urls.py
+++ HealthHub/urls.py
@@ -6,6 +6,8 @@ from . import views
 app_name = 'HealthHub'
 
 urlpatterns = [
+    path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
-    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("favicon.ico")))
+    path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
+    path('success/', views.success_view, name='success'),
 ]
--- HealthHub/views.py
+++ HealthHub/views.py
@@ -1,4 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
+
+
+def home(request):
+    return render(request, '')
+
 
 def signup(request):
-    return render(request, 'HealthHub/signup.html')
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('https://8000-bend2525-yourhealthnow-kfxlnwg8spb.ws-eu63.gitpod.io/HealthHub/success/')
+    else:
+        form = UserCreationForm()
+    return render(request, 'HealthHub/signup.html', {'form': form})
+
+
+def success_view(request):
+    return render(request, 'HealthHub/success.html')
