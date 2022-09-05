39b7b4af28dc923b1ae209639091876dbd0e0780
BenD2525 <bendawson2525@gmail.com>
2022-09-05 08:35:08 +0000

    Add login page and link up navbar

--- HealthHub/urls.py
+++ HealthHub/urls.py
@@ -8,6 +8,7 @@ app_name = 'HealthHub'
 urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.signup, name='signup'),
+    path('login/', views.login, name='login'),
     path('favicon.ico', RedirectView.as_view(url=staticfiles_storage.url("favicon.ico"))),
     path('success/', views.success_view, name='success'),
 ]
--- HealthHub/views.py
+++ HealthHub/views.py
@@ -3,7 +3,7 @@ from .forms import RegistrationForm
 
 
 def home(request):
-    return render(request, 'HealthHub/index.html')
+    return render(request, 'HealthHub/home.html')
 
 
 def signup(request):
@@ -15,6 +15,17 @@ def signup(request):
     else:
         form = RegistrationForm()
     return render(request, 'HealthHub/signup.html', {'form': form})
+
+
+def login(request):
+    if request.method == 'POST':
+        form = RegistrationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('https://8000-bend2525-yourhealthnow-kfxlnwg8spb.ws-eu63.gitpod.io/')
+    else:
+        form = RegistrationForm()
+    return render(request, 'HealthHub/login.html', {'form': form})
 
 
 def success_view(request):
