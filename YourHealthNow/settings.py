5ef2ffd341d7babe4727bc48267ce89d1bdad0f4
BenD2525 <bendawson2525@gmail.com>
2022-10-28 11:10:58 +0000

    Revert to cloudinary file storage

--- YourHealthNow/settings.py
+++ YourHealthNow/settings.py
@@ -140,7 +140,7 @@ USE_TZ = True
 
 # Static and Cloudinary Settings
 STATIC_URL = '/static/'
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
